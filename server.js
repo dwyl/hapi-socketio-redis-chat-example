223d0d382bacb770f75b64b59784de05b429e2f1
Nelson <contact.nelsonic@gmail.com>
2015-07-07 23:49:12 +0100

    split out hapi server (no socket.io) test

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 var redisClient = require('./lib/redis_connection'); // RedisCloud
 var Hapi = require('hapi');
-var SocketIO = require('socket.io');
 var server = new Hapi.Server();
 
 server.connection({
@@ -8,22 +7,11 @@ server.connection({
 	port: Number(process.env.PORT || 8000)
 });
 
-var io = SocketIO.listen(server.listener);
-
-function loadMessages (req, reply) {
-  redisClient.lrange("chat", 0, -1, function(err, data) {
-    /* istanbul ignore next */
-		if(err){ console.log(err) }
-		// console.log(data);
-    reply(data);
-  });
-}
-
 server.route([
   { method: 'GET', path: '/', handler: { file: "index.html" } },
   { method: 'GET', path: '/client.js', handler: { file: './client.js' } },
   { method: 'GET', path: '/style.css', handler: { file: './style.css' } },
-  { method: 'GET', path: '/load', handler: loadMessages },
+  { method: 'GET', path: '/load', handler: require('./lib/load_messages') },
 	{ method: 'GET', path: '/socket.io.js',
 		handler: {
 			file: './node_modules/socket.io-client/socket.io.js'
@@ -32,7 +20,7 @@ server.route([
 ]);
 
 server.start(function () {
-  io.on('connection', require('./lib/chat_handler.js'));
+	require('./lib/chat')(server.listener);
 });
 
 module.exports = server;
---
+++ test/server.js
@@ -0,0 +1,51 @@
+var QUnit = require('qunitjs'); // require QUnit node.js module
+var test = QUnit.test; // stores a copy of QUnit.test
+require('qunit-tap')(QUnit, console.log); // use console.log for test output
+
+var dir    = __dirname.split('/')[__dirname.split('/').length-1];
+var file   = dir + __filename.replace(__dirname, '') + " -> ";
+
+var server = require("../server.js");
+
+test(file +" GET / returns status 200", function(Q) {
+  var done = Q.async();
+  var options = {
+    method  : "GET",
+    url     : "/"
+  };
+  console.log(options);
+  server.inject(options, function(res) {
+    Q.equal(res.statusCode, 200, 'home page loads');
+    done();
+  });
+});
+
+test(file +" GET /load returns previous messages", function(Q) {
+  var done = Q.async();
+  var options = {
+    method  : "GET",
+    url     : "/load"
+  };
+  console.log(options);
+  server.inject(options, function(res) {
+    Q.equal(res.statusCode, 200, 'previous messages received');
+    // console.log(res.payload);
+    var messages = JSON.parse(res.payload);
+    Q.ok(messages.length > 0);
+    done();
+  });
+});
+
+test(file +" Teardown > End Redis Connection & Stop Hapi Server", function(Q) {
+  var done = Q.async();
+  var uncache = require('./uncache').uncache;   // http://goo.gl/JIjK9Y - - - \\
+  var redisClient = require('../lib/redis_connection');
+  redisClient.end();     // ensure redis con closed! - \\
+  uncache('../lib/redis_connection');           // uncache redis con  - - - - \\
+  Q.equal(redisClient.connected, false);
+  server.stop();
+  done();
+});
+
+/* istanbul ignore next */
+if (typeof module !== 'undefined' && module.exports) { QUnit.load(); } // run tests
