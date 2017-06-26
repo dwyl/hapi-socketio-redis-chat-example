449e80228c7d868486404b917075e41f5b50d4b0
Zooey <zooey@lbk.org.uk>
2017-06-26 17:47:09 +0100

    adds more structure to elm file #86

--- elm/src/Main.elm
+++ elm/src/Main.elm
@@ -1,7 +1,45 @@
 module Main exposing (..)
 
-import Html exposing (text)
+import Html exposing (..)
+import Html.Attributes exposing (..)
+import Html.Events exposing (..)
 
 
 main =
-    text "Hello, World!"
+    Html.program
+        { init = init
+        , view = view
+        , update = update
+        , subscriptions = always Sub.none
+        }
+
+
+type alias Model =
+    {}
+
+
+init : ( Model, Cmd Msg )
+init =
+    ( Model, Cmd.none )
+
+
+type Msg
+    = Hello String
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+    case msg of
+        Hello string ->
+            ( Model, Cmd.none )
+
+
+view : Model -> Html Msg
+view model =
+    div []
+        [ ul [] [ li [] [ text "chat messages go here" ] ]
+        , Html.form []
+            [ input [] []
+            , button [] [ text "Send" ]
+            ]
+        ]
--- server.js
+++ server.js
@@ -2,27 +2,46 @@ var Hapi = require('hapi');
 var server = new Hapi.Server();
 
 server.connection({
-	host: '0.0.0.0',
-	port: Number(process.env.PORT)
+  host: '0.0.0.0',
+  port: Number(process.env.PORT),
 });
 
-server.register([require('inert'), require('hapi-error')], function () {
+server.register([require('inert'), require('hapi-error')], function() {
+  server.route([
+    { method: 'GET', path: '/', handler: { file: 'index.html' } },
+    // switch these two routes for a /static handler?
+    { method: 'GET', path: '/client.js', handler: { file: './lib/client.js' } },
+    { method: 'GET', path: '/style.css', handler: { file: './style.css' } },
+    {
+      method: 'GET',
+      path: '/load',
+      handler: require('./lib/load_messages').load,
+    },
+    {
+      method: 'GET',
+      path: '/elm',
+      handler: {
+        file: './elm/index.html',
+      },
+    },
+    {
+      method: 'GET',
+      path: '/js/app.js',
+      handler: {
+        file: './elm/js',
+      },
+    },
+  ]);
 
-	server.route([
-	  { method: 'GET', path: '/', handler: { file: "index.html" } },
-		// switch these two routes for a /static handler?
-	  { method: 'GET', path: '/client.js', handler: { file: './lib/client.js' } },
-	  { method: 'GET', path: '/style.css', handler: { file: './style.css' } },
-	  { method: 'GET', path: '/load',      handler: require('./lib/load_messages').load }
-	]);
-
-	server.start(function () {
-		require('./lib/chat').init(server.listener, function(){
-			// console.log('REDISCLOUD_URL:', process.env.REDISCLOUD_URL);
-			console.log('Feeling Chatty?', 'listening on: http://127.0.0.1:' + process.env.PORT);
-		});
-	});
-
+  server.start(function() {
+    require('./lib/chat').init(server.listener, function() {
+      // console.log('REDISCLOUD_URL:', process.env.REDISCLOUD_URL);
+      console.log(
+        'Feeling Chatty?',
+        'listening on: http://127.0.0.1:' + process.env.PORT
+      );
+    });
+  });
 });
 
 module.exports = server;
