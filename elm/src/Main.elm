2117c56c06d4b9879a6eb17aa27d51638f585687
finnhodgkin <finnhodgkin@gmail.com>
2017-06-27 12:11:10 +0100

    adds initial tachyon styling to the elmchat app #86 #94

--- elm/src/Main.elm
+++ elm/src/Main.elm
@@ -3,6 +3,7 @@ module Main exposing (..)
 import Html exposing (..)
 import Html.Attributes exposing (..)
 import Html.Events exposing (..)
+import Window exposing (..)
 
 
 main =
@@ -36,7 +37,7 @@ type alias MessageInput =
 
 init : ( Model, Cmd Msg )
 init =
-    ( Model "" [ Message "god" "0" "it was good", Message "belezebub" "1" "the left hand path reaps dark rewards" ] (MessageInput "" ""), Cmd.none )
+    ( Model "" [ Message "god" "15:30:00" "it was good", Message "belezebub" "15:30:00" "the left hand path reaps dark rewards" ] (MessageInput "" ""), Cmd.none )
 
 
 type Msg
@@ -52,19 +53,19 @@ update msg model =
 
 view : Model -> Html Msg
 view model =
-    div []
-        [ ul [] (List.map parseMessage model.messages)
-        , Html.form []
-            [ input [ value model.messageInput.input, Html.Attributes.placeholder model.messageInput.placeholder, onInput UpdateInput ] []
-            , button [] [ text "Send" ]
+    div [ class "helvetica"]
+        [ ul [ class "list w-100 pa0 ma0" ] (List.map parseMessage model.messages)
+        , Html.form [ class "bg-near-black w-100 fixed pa1 bottom-0"]
+            [ input [ class "pa2 ba0 w-90", value model.messageInput.input, Html.Attributes.placeholder model.messageInput.placeholder, onInput UpdateInput ] []
+            , div [ class "w-10 center dib"] [button [ class "pa2 center w-5"] [ text "Send" ]]
             ]
         ]
 
 
 parseMessage : Message -> Html Msg
 parseMessage message =
-    li []
-        [ span [] [ text message.time ]
-        , span [] [ text message.author ]
-        , p [] [ text message.message ]
+    li [ class "pv3 ph3 striped--light-gray"]
+        [ span [ class "gray f5" ] [ text message.time ]
+        , span [ class "blue mh1 f5"] [ text message.author ]
+        , p [ class "mv1 f3"] [ text message.message ]
         ]
