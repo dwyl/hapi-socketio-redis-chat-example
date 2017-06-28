3604f97dd2fe2d126b3453cd4480a684967b0fa8
finnhodgkin <finnhodgkin@gmail.com>
2017-06-28 10:25:39 +0100

    adds a name input and saves the name to localstorage through a port #86

--- elm/src/Main.elm
+++ elm/src/Main.elm
@@ -1,4 +1,4 @@
-module Main exposing (..)
+port module Main exposing (..)
 
 import Html exposing (..)
 import Html.Attributes exposing (..)
@@ -8,7 +8,7 @@ import Window exposing (..)
 
 
 main =
-    Html.program
+    Html.programWithFlags
         { init = init
         , view = view
         , update = update
@@ -20,6 +20,7 @@ type alias Model =
     { name : String
     , messages : List Message
     , messageInput : MessageInput
+    , nameInput : String
     , windowWidth : Int
     }
 
@@ -37,14 +38,22 @@ type alias MessageInput =
     }
 
 
-init : ( Model, Cmd Msg )
-init =
-    ( Model "" [ Message "god" "15:30:00" "it was good", Message "belezebub" "15:30:00" "the left hand path reaps dark rewards", Message "god" "15:30:00" "it was good", Message "belezebub" "15:30:00" "the left hand path reaps dark rewards", Message "god" "15:30:00" "it was good", Message "belezebub" "15:30:00" "the left hand path reaps dark rewards", Message "god" "15:30:00" "it was good", Message "belezebub" "15:30:00" "the left hand path reaps dark rewards", Message "god" "15:30:00" "it was good", Message "belezebub" "15:30:00" "the left hand path reaps dark rewards", Message "god" "15:30:00" "it was good", Message "belezebub" "15:30:00" "the left hand path reaps dark rewards", Message "god" "15:30:00" "it was good", Message "belezebub" "15:30:00" "the left hand path reaps dark rewards", Message "god" "15:30:00" "it was good", Message "belezebub" "15:30:00" "the left hand path reaps dark rewards" ] (MessageInput "" "") 0, Task.perform Resize Window.width )
+init : Maybe String -> ( Model, Cmd Msg )
+init name =
+    case name of
+      Just name ->
+        ( Model name [ Message "god" "15:30:00" "it was good" ] (MessageInput "" "") "" 0, Task.perform Resize Window.width )
+      Nothing ->
+        ( Model "" [ Message "god" "15:30:00" "it was good" ] (MessageInput "" "") "" 0, Task.perform Resize Window.width )
+
+
 
 
 type Msg
     = UpdateInput String
+    | UpdateNameInput String
     | Resize Int
+    | SetName
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
@@ -53,29 +62,57 @@ update msg model =
         UpdateInput message ->
             ( { model | messageInput = MessageInput message "" }, Cmd.none )
 
+        UpdateNameInput name ->
+            ( { model | nameInput = name }, Cmd.none )
+
         Resize newWidth ->
             ( { model | windowWidth = newWidth }, Cmd.none )
+
+        SetName ->
+            ( { model | name = model.nameInput, nameInput = "" }, setName model.nameInput )
 
 
 view : Model -> Html Msg
 view model =
-    div [ class "helvetica" ]
-        [ ul [ class "list w-100 pt0 pl0 pr0 pb5rem ma0" ] (List.map parseMessage model.messages)
-        , Html.form [ class "bg-near-black h3_5 w-100 bw2 fixed bottom-0 pt2" ]
-            [ input [ class "fixed bottom-1 left-1 ba0 f3 pv2 border-box", Html.Attributes.style [ ( "width", toString (model.windowWidth - 148) ++ "px" ) ], value model.messageInput.input, Html.Attributes.placeholder model.messageInput.placeholder, onInput UpdateInput ] []
-            , button [ class "fixed bottom-1 right-1 fr ba0 ph1 f3 pv2 white border-box", Html.Attributes.style [ ( "width", "103px" ), ( "background-color", "#4DB6AC" ), ( "border-color", "#4DB6AC" ) ] ] [ text "Send" ]
-            ]
-        ]
+  case model.name of
+    "" ->
+      login model
+    _ ->
+      chat model
 
+chat: Model -> Html Msg
+chat model =
+  div [ class "helvetica" ]
+      [ ul [ class "list w-100 pt0 pl0 pr0 pb5rem ma0" ] (List.map parseMessage model.messages)
+      , Html.form [ class "bg-near-black h3_5 w-100 bw2 fixed bottom-0 pt2" ]
+          [ input [ class "fixed bottom-1 left-1 ba0 f3 pv2 border-box", Html.Attributes.style [ ( "width", toString (model.windowWidth - 148) ++ "px" ) ], value model.messageInput.input, Html.Attributes.placeholder model.messageInput.placeholder, onInput UpdateInput ] []
+          , button [ class "fixed bottom-1 right-1 fr ba0 ph1 f3 pv2 white border-box", Html.Attributes.style [ ( "width", "103px" ), ( "background-color", "#4DB6AC" ), ( "border-color", "#4DB6AC" ) ] ] [ text "Send" ]
+          ]
+      ]
+
+login: Model -> Html Msg
+login model =
+  Html.form [ class "pa4 black-80", onSubmit SetName ]
+    [ div [ class "measure" ]
+        [ label [ class "f6 b db mb2", for "name" ]
+            [ text "Name" ]
+        , input [ attribute "aria-describedby" "name-desc", class "input-reset ba b--black-20 pa2 mb2 db w-100", id "name", type_ "text", value model.nameInput, onInput UpdateNameInput ]
+            []
+        , small [ class "f6 black-60 db mb2", id "name-desc" ]
+            [ text "Helper text for the form control." ]
+        ]
+    ]
 
 parseMessage : Message -> Html Msg
 parseMessage message =
     li [ class "pv3 ph3 striped--light-gray" ]
-        [ span [ class "gray f5" ] [ text message.time ]
-        , span [ class "blue mh1 f5" ] [ text message.author ]
-        , p [ class "mv1 f3" ] [ text message.message ]
+        [ span [ class "gray f6 f5-m f4-l" ] [ text message.time ]
+        , span [ class "blue mh1 f6 f5-m f4-l" ] [ text message.author ]
+        , p [ class "mv1 f5 f4-m f3-l" ] [ text message.message ]
         ]
 
 
 subscriptions model =
     Window.resizes (\{ height, width } -> Resize width)
+
+port setName : String -> Cmd msg
